e92acd646659071730ed93d6071a8426ba764c57
Jafet Morales <jafet.aaron.morales@gmail.com>
2021-06-09 10:56:58 -0500

    Add files via upload

---
+++ Example use/test (1).py
@@ -0,0 +1,186 @@
+import cv2
+import argparse
+import numpy as np
+
+LABLE = []
+CONFIDENT = []
+
+# function to get the output layer names 
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+# function to draw bounding box on the detected object with class name
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+    color = COLORS[class_id]
+    #print(label,confidence)
+    LABLE.append(label)
+    CONFIDENT.append(confidence)
+    cv2.rectangle(img, (int(x),int(y)), (int(x_plus_w),int(y_plus_h)), color, 2)
+    cv2.putText(img, label+":"+str(confidence), (int(x-10),int(y-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+########################### read input image ###########################################
+
+## argparse section
+ap = argparse.ArgumentParser()
+ap.add_argument("-n","--name",required=True,help="name of the file")
+args = vars(ap.parse_args())
+image = cv2.imread(args["name"])
+
+## image name section
+#image = cv2.imread("name.jpg")
+
+Width = image.shape[1]
+Height = image.shape[0]
+scale = 0.00392
+
+
+
+
+
+############################## Check ears o the pig #####################################
+classes = None
+with open("pig.names", 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# generate different colors for different classes 
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+# read pre-trained model and config file
+net = cv2.dnn.readNet("pig_f_b_as.weights", "pig_f_b_as.cfg")
+# create input blob 
+blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+# set input blob for the network
+net.setInput(blob)
+# run inference through the network
+# and gather predictions from output layers
+outs = net.forward(get_output_layers(net))
+# initialization
+class_ids = []
+confidences = []
+boxes = []
+conf_threshold = 0.5
+nms_threshold = 0.4
+
+# for each detetion from each output layer 
+# get the confidence, class id, bounding box params
+# and ignore weak detections (confidence < 0.5)
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            center_x = int(detection[0] * Width)
+            center_y = int(detection[1] * Height)
+            w = int(detection[2] * Width)
+            h = int(detection[3] * Height)
+            x = center_x - w / 2
+            y = center_y - h / 2
+            class_ids.append(class_id)
+            confidences.append(float(confidence))
+            boxes.append([x, y, w, h])
+
+# apply non-max suppression
+indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+# go through the detections remaining
+# after nms and draw bounding box
+for i in indices:
+    i = i[0]
+    box = boxes[i]
+    x = box[0]
+    y = box[1]
+    w = box[2]
+    h = box[3]
+    #print(x, y, w, h)
+    draw_bounding_box(image, class_ids[i], confidences[i], int(x), int(y), int(x+w), int(y+h))
+
+
+
+
+classes = None
+with open("Pig_su.names", 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# generate different colors for different classes 
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+# read pre-trained model and config file
+net1 = cv2.dnn.readNet("pig_side_up.weights", "pig_side_up.cfg")
+# create input blob 
+blob1 = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+# set input blob for the network
+net1.setInput(blob1)
+# run inference through the network
+# and gather predictions from output layers
+outs1 = net1.forward(get_output_layers(net1))
+# initialization
+class_ids = []
+confidences = []
+boxes = []
+conf_threshold = 0.5
+nms_threshold = 0.4
+
+# for each detetion from each output layer 
+# get the confidence, class id, bounding box params
+# and ignore weak detections (confidence < 0.5)
+for out in outs1:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            center_x = int(detection[0] * Width)
+            center_y = int(detection[1] * Height)
+            w = int(detection[2] * Width)
+            h = int(detection[3] * Height)
+            x = center_x - w / 2
+            y = center_y - h / 2
+            class_ids.append(class_id)
+            confidences.append(float(confidence))
+            boxes.append([x, y, w, h])
+
+# apply non-max suppression
+indices1 = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+# go through the detections remaining
+# after nms and draw bounding box
+for i in indices1:
+    i = i[0]
+    box = boxes[i]
+    x = box[0]
+    y = box[1]
+    w = box[2]
+    h = box[3]
+    #print(x, y, w, h)
+    draw_bounding_box(image, class_ids[i], confidences[i], int(x), int(y), int(x+w), int(y+h))
+    
+    
+
+print(LABLE[CONFIDENT.index(max(CONFIDENT))])
+    
+# display output image    
+cv2.imshow("detection", image)
+cv2.waitKey()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
